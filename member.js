14840ff19ca661c3d0f12dfda32b2291350e767f
Wysper <85033182+WysperOtaku@users.noreply.github.com>
2023-11-29 16:29:45 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,13 +1,3 @@
 function skillMember(){
-    var member = document.getElementById("member");
-    var memberValue = member.value;
-    if(memberValue.length == 0){
-        document.getElementById("memberError").innerHTML = "Please enter member";
-        member.style.border = "1px solid red";
-        return false;
-    }else{
-        document.getElementById("memberError").innerHTML = "";
-        member.style.border = "1px solid #ced4da";
-        return true;
-    }
+    console.log('skillMember');
 }
