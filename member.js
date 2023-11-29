4bf47d49f1f5cc28517381eec3990e3919d6c749
Wysper <85033182+WysperOtaku@users.noreply.github.com>
2023-11-29 16:34:00 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,14 @@
-function skillMember(){
-    console.log('skillMember');
+function skillMember() {
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var project = document.getElementById("project");
+    var memberContent = document.getElementById("memberContent");
+    var skillContent = document.getElementById("skillContent");
+    var projectContent = document.getElementById("projectContent");
+    member.style.color = "#000";
+    skill.style.color = "#aaa";
+    project.style.color = "#aaa";
+    memberContent.style.display = "block";
+    skillContent.style.display = "none";
+    projectContent.style.display = "none";
 }
